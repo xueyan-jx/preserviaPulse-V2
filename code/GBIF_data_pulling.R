9e6c6af2ec9ad39621c707cc89e95d696b25d5ae
Wenxin Yang <wxyang.wenxin@gmail.com>
2025-04-23 11:38:51 -0700

    Squashed commit of the following:
    commit b22cef16de8ddaf6634e041641c1ec3c2cff05b3
    Author: xueyan-jx <yanxue171@gmail.com>
    Date:   Wed Apr 23 11:32:11 2025 -0700
    
        code update for getting all records
    
    commit f194c0a6504f2692a2e3dfd713915d496ed83691
    Merge: 504ab3b a3f5315
    Author: xueyan-jx <yanxue171@gmail.com>
    Date:   Wed Apr 23 11:29:05 2025 -0700
    
        Update GBIF_data_pulling.R
    
    commit a3f531565aad5ab710905161cbc7c68388c72c60
    Author: xueyan-jx <yanxue171@gmail.com>
    Date:   Wed Apr 23 11:13:46 2025 -0700
    
        update code for getting occurrence
    
    commit 08308e49684490b8a0b5782b1843b4a1e5955960
    Author: xueyan-jx <yanxue171@gmail.com>
    Date:   Wed Apr 23 09:54:42 2025 -0700
    
        get occurrence - xue
    
    commit e514401c122f2283288ee029e7a72b5b1e02ce0e
    Author: xueyan-jx <yanxue171@gmail.com>
    Date:   Wed Apr 23 09:54:03 2025 -0700
    
        delete this one
    
    commit 504ab3b43f089f836b79f46999ac591d736d97b1
    Author: Lei Song <pinot.sung@gmail.com>
    Date:   Mon Apr 21 20:40:58 2025 -0700
    
        Reformat the repo
    
    commit 7f005f15baff191229edabdc3dbbb1e92596a6f8
    Merge: 516e5e9 c828637
    Author: Jacqui Vogel <jacquivogel@gmail.com>
    Date:   Mon Apr 21 12:56:39 2025 -0700
    
        Merge branch 'main' of https://github.com/ucsbscale/preserviaPulse
    
    commit 516e5e9a5fc7fef31e249b4b40323306ee4c95a8
    Author: Jacqui Vogel <jacquivogel@gmail.com>
    Date:   Mon Apr 21 12:56:25 2025 -0700
    
        GBIF API script
    
    commit fe0d347fab33a6fa8c3ebc8d8bf1e6c2bc2b1ac0
    Author: xueyan-jx <yanxue171@gmail.com>
    Date:   Mon Apr 21 12:07:40 2025 -0700
    
        test
    
    commit c828637fff4e042fe3c8ec9a796b5c7b53638552
    Author: Lei Song <pinot.sung@gmail.com>
    Date:   Mon Apr 21 11:56:32 2025 -0700
    
        ADD my name

--- code/GBIF_data_pulling.R
+++ code/GBIF_data_pulling.R
@@ -25,7 +25,7 @@ if (!dir.exists(occ_dir)) dir.create(occ_dir)
 # Confirm names used in GBIF database for your species list
 scientific_names <- c("Deinandra increscens ssp. Villosa",
                       "Eriodictyon capitatum")
-gbif_names <- name_backbone_checklist(scientific_names)
+gbif_names <- name_backbone_checklist(dat$`Name (Latin)`)
 
 # But if you don't know the exact scientific names
 nm <- "Grasshopper sparrow"
@@ -63,6 +63,73 @@ occ_meta <- occ_download(
   pwd = "pwd",       # Replace with your actual password
   email = "email"  # Replace with your registered email
 )
+
+# Save out the name catalog using the GBIF query unique ID in name to pair.
+write.csv(gbif_names, 
+          file.path(occ_dir, sprintf("gbif_names_%s.csv", occ_meta)),
+          row.names = FALSE)
+
+# Download the occurrences
+status <- occ_download_wait(occ_meta)
+if (status$status == "SUCCEEDED"){
+  d <- occ_download_get(occ_meta, path = occ_dir, overwrite = TRUE)
+} else {
+  warning(sprintf("The request %s.", status$status))
+  d <- NULL
+}
+
+
+
+################ If want to get occurrence for a species list #######
+################       Same settings as above        ################
+################       1. Get species list   ########################
+library(googledrive)
+library(googlesheets4)
+
+# Authoritization
+drive_auth()
+gs4_auth(token = drive_token())
+
+# Get data
+ss <- drive_get("Special Status Species")
+
+## E.g. sheet names
+ss_meta <- gs4_get(ss) 
+sheet_names <- ss_meta$sheets$name
+
+dat <- read_sheet(ss, sheet = sheet_names[1])
+head(dat)
+
+# Get species' names
+scientific_names_df <- dat[, c("Name (common)", "Name (Latin)")]
+
+# Use Latin name for searching
+scientific_names <- scientific_names_df$`Name (Latin)`
+
+
+################# 2. Get information from gbif ######################
+# Confirm names used in GBIF database for your species list
+gbif_names <- name_backbone_checklist(scientific_names)
+
+# Only keep valid records
+gbif_names <- gbif_names[!is.na(gbif_names$usageKey), ]
+
+# Spin up a download request for GBIF occurrence data
+# Reference
+# https://docs.ropensci.org/rgbif/articles/getting_occurrence_data.html
+# https://data-blog.gbif.org/post/downloading-long-species-lists-on-gbif/
+
+occ_meta <- occ_download(
+  pred_in("taxonKey", unique(gbif_names$usageKey)),
+  pred("occurrenceStatus", "PRESENT"), 
+  pred("hasCoordinate", TRUE), pred("hasGeospatialIssue", FALSE),
+  pred_not(pred_in("basisOfRecord",
+                   c("FOSSIL_SPECIMEN","LIVING_SPECIMEN"))),
+  format = "SIMPLE_CSV",
+  user = "user",      # Replace with your actual username
+  pwd = "pwd",       # Replace with your actual password
+  email = "email"  # Replace with your registered email
+  )
 
 # Save out the name catalog using the GBIF query unique ID in name to pair.
 write.csv(gbif_names, 
