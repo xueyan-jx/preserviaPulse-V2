637567d987cdf4b96b362af1fe621f9796918471
xueyan-jx <yanxue171@gmail.com>
2025-05-19 12:00:28 -0700

    sdm test code

--- code/test.R
+++ code/test.R
@@ -1,25 +1,31 @@
 library(SSDM)
 library(raster)
 library(here)
-df <- read.csv(here('data/occurrences/animals/birds_cleaned_0515.csv'), sep=';')
+library(dplyr)
+library(readr)
+
+df <- read.csv(here('data/occurrences/GBIF_BIEN_DP_final.csv'), sep = ",")
 colnames(df)
 
 options(digits = 16)
+
 df <- df %>%
   mutate(
     coords = gsub("c\\(|\\)", "", geometry),  # remove c() wrapper
     x = as.double(sapply(strsplit(coords, ","), `[`, 1)),  # first coord
     y = as.double(sapply(strsplit(coords, ","), `[`, 2))   # second coord
   ) 
-write.table(df, 'data/occurrences/animals/birds_cleaned_0515_xy.csv', sep=';')
+
+df <- df[, c("x", "y", "species", "scientificName")]
+readr::write_csv(df, 'data/occurrences/plant_for_model_test.csv')
 
 
-Env <- load_var(path = 'data/env', format = '.tif', verbose = FALSE)
+Env <- load_var(path = 'data/env/', format = '.tif', verbose = FALSE)
 Env
 
-Occ <- load_occ(path = 'data/occurrences/animals', Env,
+Occ <- load_occ(path = 'data/occurrences', Env,
                 Xcol = 'x', Ycol = 'y',
-                file = 'birds_cleaned_0515_xy.csv', sep = ';', verbose = FALSE)
+                file = 'plant_for_model_test.csv', verbose = FALSE)
 head(Occ)
 
 
